6811df78cd59b803ab472ac4583a4e6dd1098790
git.anirudh@icloud.com <janirudh2005@gmail.com>
2020-09-23 15:57:36 -0500

    api & levels

---
+++ src/api.ts
@@ -0,0 +1,4 @@
+import { Logger } from "./logger";
+import { LogLevel } from "./levels"
+
+export const createLogger = () => new Logger(LogLevel.DEBUG);
---
+++ src/levels.ts
@@ -0,0 +1,13 @@
+export enum LogLevel {
+    DEBUG = 0,
+    INFO,
+    WARN,
+    ERROR,
+    SILENT,
+}
+
+export const DEBUG = LogLevel.DEBUG;
+export const INFO = LogLevel.INFO;
+export const WARN = LogLevel.WARN;
+export const ERROR = LogLevel.ERROR;
+export const SILENT = LogLevel.SILENT;
