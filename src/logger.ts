9b0cafc196a63d539e6330c3f874ca3b570688fd
git.anirudh@icloud.com <janirudh2005@gmail.com>
2020-09-23 15:57:30 -0500

    main logger class

---
+++ src/logger.ts
@@ -0,0 +1,82 @@
+import { LogLevel } from './levels';
+
+export class Logger {
+    private logLevel: LogLevel;
+
+    constructor(logLevel: LogLevel) {
+        this.logLevel = logLevel;
+    }
+
+    public getLevel() {
+        return this.logLevel;
+    }
+
+    public setLevel(level: LogLevel) {
+        this.logLevel = level;
+    }
+
+    public doNothing() {
+        // do not log
+    }
+
+    public debug() {
+        return this.logLevel <= LogLevel.DEBUG
+            ? this.bindToFunc({
+                  name: 'DEBUG',
+                  method: console.log,
+                  background: 'rgba(0,0,0,0.1)',
+              })
+            : this.doNothing;
+    }
+
+    public info() {
+        return this.logLevel <= LogLevel.INFO
+            ? this.bindToFunc({
+                  name: 'INFO',
+                  method: console.log,
+                  background: 'rgba(0,0,0,0.1)',
+              })
+            : this.doNothing;
+    }
+
+    public warn() {
+        return this.logLevel <= LogLevel.WARN
+            ? this.bindToFunc({
+                  name: 'INFO',
+                  method: console.log,
+                  background: '#FB8C00',
+              })
+            : this.doNothing;
+    }
+
+    public error() {
+        return this.logLevel <= LogLevel.ERROR
+            ? this.bindToFunc({
+                  name: 'ERROR',
+                  method: console.log,
+                  background: 'red',
+              })
+            : this.doNothing;
+    }
+
+    public assert() {
+        return this.bindToFunc({
+            name: 'ASSERT',
+            method: console.assert,
+            background: 'red',
+        });
+    }
+
+    private bindToFunc(config: {
+        name: string;
+        method: any;
+        background: string;
+    }) {
+        return Function.prototype.bind.call(
+            config.method,
+            console,
+            `%c${config.name}%c`,
+            `font-size:10px;background:${config.background};color:white;padding:3px 6px`
+        );
+    }
+}
