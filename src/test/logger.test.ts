9a7a06f9aeba73d7757d363e8ca2a9125bc46c25
Anirudh Janga <j.anirudh@icloud.com>
2020-09-24 21:25:06 -0500

    Adding a simple test

--- src/test/logger.test.ts
+++ src/test/logger.test.ts
@@ -1,5 +1,8 @@
+import { createLogger, DEBUG } from "src/api";
+
 describe("Basic Tests", () => {
-    it("Should create Logger successfuly", () => {
-        expect(1).toBe(1);
+    it("Should create Logger successfuly with DEBUG level", () => {
+        const logger = createLogger();
+        expect(logger.getLevel()).toBe(DEBUG);
     });
 });
